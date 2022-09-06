f5f32a5ffe28e75e4a0648b1f5c267ec42c8ae8c
andres06 <andressierrarojas7@gmail.com>
2022-09-06 13:21:31 -0500

    CREATED: interface Room and User

--- server/src/interfaces/Room.ts
+++ server/src/interfaces/Room.ts
@@ -1,4 +1,4 @@
 export interface DataRoom {
-    idRoom:string;
-    owner:string;
+    _idRoom:string;
+    _owner:string;
 }
--- server/src/interfaces/User.ts
+++ server/src/interfaces/User.ts
@@ -1,6 +1,7 @@
+import { DataCards } from './Cards';
+
 export interface DataUser {
-    _id:string;
     _name:string;
     _idRoom:string;
-    _cards:Array<object>;
+    _cards?:Array<DataCards>;
 }
