e3dbcf29e697c2870533350db9ef750f1546b485
andres06 <andressierrarojas7@gmail.com>
2022-09-06 16:01:25 -0500

    MODIFIED

--- server/src/controllers/createRoom.controller.ts
+++ server/src/controllers/createRoom.controller.ts
@@ -3,19 +3,19 @@ import { getResponse } from "../services/getResponse.service";
 import { createIdRoom, saveRoomService } from "../services/room.service";
 
 
-export const createRoom = async (data:DataRoom, callback:any) => {
-	//Create id 
-	const _idRoom:string = createIdRoom();
-    const _owner:string = data._owner;
-    //save room
-    const result:boolean|DataRoom = await saveRoomService({_idRoom,_owner});
-    //Validamos resultado
-    if(!result) return callback({
-    //Invalid response server - error server
-    ...getResponse(502)
-    });
-    //Else
-    return callback({
-    ...getResponse(200)
-    });
-};
+// export const createRoom = async (data:DataRoom, callback:any) => {
+// 	//Create id 
+// 	const _idRoom:string = createIdRoom();
+//     const _owner:string = data._owner;
+//     //save room
+//     const result:boolean|DataRoom = await saveRoomService({_idRoom,_owner,_namesUsers:[]});
+//     //Validamos resultado
+//     if(!result) return callback({
+//     //Invalid response server - error server
+//     ...getResponse(502)
+//     });
+//     //Else
+//     return callback({
+//     ...getResponse(200)
+//     });
+// };
