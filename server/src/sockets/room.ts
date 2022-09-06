f435293a2f151e82d160b0d043ceb94d95af3ab3
andres06 <andressierrarojas7@gmail.com>
2022-09-06 15:21:41 -0500

    MODIFIED: se modifica sockets create Room

--- server/src/sockets/room.ts
+++ server/src/sockets/room.ts
@@ -29,21 +29,21 @@ export const game = (io:Server):void =>{
             });
             //?Create room 
             socket.on('room:create', async (data:DataRoom, callback)=>{
-                // //Create id 
-                // const _idRoom:string = createIdRoom();
-                // const _owner:string = data._owner;
-                // //save room
-                // const result:boolean|DataRoom = await saveRoomService({_idRoom,_owner});
-                // //Validamos resultado
-                // if(!result) return callback({
-                //     //Invalid response server - error server
-                //     ...getResponse(502)
-                // });
-                // //Else
-                // return callback({
-                //     ...getResponse(200)
-                // });
-                await createRoom(data,callback);
+                //Create id 
+                const _idRoom:string = createIdRoom();
+                const _owner:string = data._owner;
+                //save room
+                const result:boolean|DataRoom = await saveRoomService({_idRoom,_owner});
+                //Validamos resultado
+                if(!result) return callback({
+                    //Invalid response server - error server
+                    ...getResponse(502)
+                });
+                //Else
+                return callback({
+                    ...getResponse(200)
+                });
+                // await createRoom(data,callback);
             });
             //?Create User
             socket.on('user:create', async (data:DataUser, callback) => {
